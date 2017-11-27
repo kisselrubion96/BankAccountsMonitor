33ebc1fa29f488150602e54afdb8b4a315854245
kisselrubion96 <34016009+kisselrubion96@users.noreply.github.com>
2017-11-27 10:31:55 +0800

    Add files via upload
    added function on node.js

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener("ready",on_device_ready,false);
+
+function on_device_ready()
+{
+
+}
+function on_add_account()
+{
+    var inputAmount = document.getElementById("inputAmount").value;
+    alert(inputAmount);
+
+} 
