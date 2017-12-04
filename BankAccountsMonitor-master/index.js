b50a344949f21916c2e732713c341e7d2edc5024
Kissel Rubion <34016009+kisselrubion96@users.noreply.github.com>
2017-12-04 10:46:12 +0800

    Changed UI Design
    separated css file
    edited javascript files

---
+++ BankAccountsMonitor-master/index.js
@@ -0,0 +1,17 @@
+document.addEventListener("ready",on_device_ready,false);
+
+function on_device_ready()
+{
+
+}
+function on_add_account()
+{
+    //var inputAmount = document.getElementById("inputAmount").value;
+    //alert(inputAmount);
+	//console.log("asdasdasd");
+	
+	//try{document.getElementById("newTab").style.height = "250px";}
+	//catch(e){ console.log(e);}
+	document.getElementById("newTab").style.display = "none";
+	document.getElementById("accountTab").style.visibility = "visible";
+} 
